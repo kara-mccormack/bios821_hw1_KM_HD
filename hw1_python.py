f16c6c160a74fb9f2dba1fa3e91508d4226f8890
Kara McCormack <karamccormack@Karas-MacBook-Pro.local>
2022-01-17 17:02:06 -0500

    initial commit, write skeleton of both functions

---
+++ hw1_python.py
@@ -0,0 +1,20 @@
+import math 
+
+def get_data(file):
+    """Simple python function to get data"""
+    # read in a .txt file
+    # return a list of lists of integers
+    out = 2
+    return out
+
+def analyze_data(numbers, function):
+    """Simple python function to operate on list of integers"""
+    if function == "average":
+        x = # take the mean of all numbers
+    if function == "standard deviation":
+        x = # take std deviation of all numbers
+    if function == "covariance":
+        x = # take covar
+    if function == "correlation":
+        x = # take covar
+    return float(x)
